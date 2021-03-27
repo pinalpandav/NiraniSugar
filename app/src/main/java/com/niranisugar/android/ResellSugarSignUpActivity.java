9d7abf881c9a14422013738ac8ade8af65efcb90
netratechnosys <dharmeshghoghari@live.com>
2021-03-27 19:50:37 +0530

    27th March 2021

---
+++ app/src/main/java/com/niranisugar/android/ResellSugarSignUpActivity.java
@@ -0,0 +1,23 @@
+package com.niranisugar.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ResellSugarSignUpActivity extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_resell_sugar);
+
+
+    }
+
+
+
+
+}
