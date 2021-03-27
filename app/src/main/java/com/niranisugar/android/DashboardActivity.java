1c0a774e926c4beb75282aaaabaf86c708750938
netratechnosys <dharmeshghoghari@live.com>
2021-03-27 14:09:42 +0530

    27th March 2021

--- app/src/main/java/com/niranisugar/android/DashboardActivity.java
+++ app/src/main/java/com/niranisugar/android/DashboardActivity.java
@@ -20,7 +20,6 @@ public class DashboardActivity extends Activity {
    RecyclerView rvCategories,rvFeatured,rvBestSell;
    public ArrayList<CategoriesModel> arrCategories = new ArrayList<>();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +50,6 @@ public class DashboardActivity extends Activity {
         LinearLayoutManager llm = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
         rvCategories.setLayoutManager(llm);
 
-        // TODO: 05-01-2021 Set data in adapter
         CategoriesAdapter categoriesAdapter = new CategoriesAdapter(this, "Fragment", arrCategories, "");
         rvCategories.setAdapter(categoriesAdapter);
 
@@ -60,7 +58,6 @@ public class DashboardActivity extends Activity {
         LinearLayoutManager llmF = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
         rvFeatured.setLayoutManager(llmF);
 
-        // TODO: 05-01-2021 Set data in adapter
         FeaturedAdapter featuredAdapter = new FeaturedAdapter(this, "Fragment", arrCategories, "");
         rvFeatured.setAdapter(featuredAdapter);
 
@@ -69,15 +66,11 @@ public class DashboardActivity extends Activity {
         LinearLayoutManager llmBS = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
         rvBestSell.setLayoutManager(llmBS);
 
-        // TODO: 05-01-2021 Set data in adapter
         FeaturedAdapter bestSellAdapter = new FeaturedAdapter(this, "Fragment", arrCategories, "");
         rvBestSell.setAdapter(bestSellAdapter);
-
-
     }
 
     private void findViews() {
-
         rvCategories = findViewById(R.id.rvCategories);
         rvFeatured = findViewById(R.id.rvFeatured);
         rvBestSell = findViewById(R.id.rvBestSell);
--- app/src/main/java/com/niranisugar/android/LoginActivity.java
+++ app/src/main/java/com/niranisugar/android/LoginActivity.java
@@ -3,13 +3,22 @@ package com.niranisugar.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.cardview.widget.CardView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends Activity {
 
@@ -18,6 +27,8 @@ public class LoginActivity extends Activity {
     ImageView btnBack;
     TextView btnRegisterNow;
     CardView btnLogin;
+    CardView btnLoginWithGoogle;
+    public static final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +77,61 @@ public class LoginActivity extends Activity {
                 startActivity(new Intent(LoginActivity.this,MainActivity.class));
             }
         });
+
+        btnLoginWithGoogle.setOnClickListener(view -> {
+            signInwithGoogle();
+        });
+    }
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    protected void signInwithGoogle(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            Toast.makeText(this, "Login Success " + account.getEmail(), Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }else{
+            Toast.makeText(this, "Login Fail ", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void findViews() {
@@ -79,6 +145,8 @@ public class LoginActivity extends Activity {
         btnRegisterNow = findViewById(R.id.btnRegister);
 
         btnLogin = findViewById(R.id.btnLogin);
+
+        btnLoginWithGoogle = findViewById(R.id.btnLoginWithGoogle);
 
     }
 }
